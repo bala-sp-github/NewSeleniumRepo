7d18fb803a6e57888872f0eb62d3177981f9f1a2
bala-sp-github <40242683+bala-sp-github@users.noreply.github.com>
2018-12-21 07:28:22 +0530

    Update notificationappTest.java

--- notificationapp-test/src/test/java/testNG/notificationappTest.java
+++ notificationapp-test/src/test/java/testNG/notificationappTest.java
@@ -4,26 +4,28 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 public class notificationappTest {
 	WebDriver driver;
 	
-	@BeforeMethod
+	@BeforeTest
 	public void launch() {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("http://172.17.0.2:8080/notificationapp/");
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
   @Test
   public void verifylogin() {
 	  driver.manage().window().maximize();
-	  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	  //driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 	  driver.findElement(By.xpath("//div[@class='button-container']")).click();
   }
   
-  @AfterMethod
+@AfterTest
   public void logout() {
 	  driver.close();
   }
